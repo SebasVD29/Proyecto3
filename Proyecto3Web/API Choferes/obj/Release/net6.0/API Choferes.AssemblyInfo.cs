3c685c08e20f5bd1a1af15e0e61f52098aebe8e4
SebasVD29 <100324444+SebasVD29@users.noreply.github.com>
2023-10-09 22:35:20 -0600

    CambioColor y arreglos

--- Proyecto3Web/API Choferes/obj/Release/net6.0/API Choferes.AssemblyInfo.cs
+++ Proyecto3Web/API Choferes/obj/Release/net6.0/API Choferes.AssemblyInfo.cs
@@ -11,7 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("API Choferes")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("API Choferes")]
+[assembly: System.Reflection.AssemblyTitleAttribute("API Choferes")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
