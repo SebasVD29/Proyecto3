a4a2db02dc86c5d702b998a3b17b907cf1c216a9
SebasVD29 <100324444+SebasVD29@users.noreply.github.com>
2023-10-09 19:33:04 -0600

    API local

---
+++ Proyecto3Web/API Choferes/Controllers/choferesControllers.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Http.Cors;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Choferes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors(origins: "*", methods: "*", headers: "*")]
+    public class choferesControllers : ControllerBase
+    {
+        int count = 0;
+        SqlConnection sqlConnection;
+
+        // GET: api/<choferesControllers>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {         
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<choferesControllers>/5
+        [HttpGet("{id}")]
+        public string[] Get(int id)
+        {
+            string sqlconn = $"Server=tcp:proyecto3-server.database.windows.net,1433;Initial Catalog=Plogistics;Persist Security Info=False;User ID=adminp;Password=Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            sqlConnection = new SqlConnection(sqlconn);
+
+            try
+            {
+                string[] returnValues = new string[100];
+                int counter = 0;
+                sqlConnection.Open();
+                string queryString = "Select * from dbo.Chofer where IdentificadorChofer =" + id;
+                SqlCommand command = new SqlCommand(queryString, sqlConnection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    return new string[] { (string)reader["Nombre"], (string)reader["Email"] };
+
+                }
+                reader.Close();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return new string[] { "error", "error" };
+        }
+
+        // POST api/<choferesControllers>
+        [HttpPost]
+        [EnableCors(origins: "*", methods: "*", headers: "*")]
+        public void Post(int identificacion, string nombre, string apellidos, string email, string contrasena, string fecha, string estado)
+        {
+            string sqlconn = $"Server=tcp:proyecto3-server.database.windows.net,1433;Initial Catalog=Plogistics;Persist Security Info=False;User ID=adminp;Password=Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            sqlConnection = new SqlConnection(sqlconn);
+
+            try
+            {
+                string[] returnValues = new string[100];
+                sqlConnection.Open();
+                string queryString = 
+                    "INSERT INTO dbo.Chofer(IdentificadorChofer, Nombre, Apellido, Email, Contraseña, FechaRegistro, Estado) " +
+                    "VALUES (" + identificacion + ",'" + nombre + "','" + apellidos + "','" + email + "','" +contrasena + "','" + fecha + "','" + estado + "');";
+                SqlCommand command = new SqlCommand(queryString, sqlConnection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                { }
+                reader.Close();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return;
+        }
+
+        // PUT api/<choferesControllers>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<choferesControllers>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Proyecto3Web/API Choferes/Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+//Cors
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("", "null", "*", "https://www.google.com").AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                      });
+});
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//cors
+app.UseCors(MyAllowSpecificOrigins);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
