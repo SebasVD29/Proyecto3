aa0c6a14fab1e0caa134b366b96077802f79526f
frankyhernandez <julihr2708@outlook.com>
2023-10-16 20:15:47 +0000

    cambios franky incompletos

--- Proyecto3Web/carint-html/js/logica.js
+++ Proyecto3Web/carint-html/js/logica.js
@@ -22,13 +22,13 @@ var estado = $("#form-estado").children(":selected")[0].label;
 
  jQuery.ajax({
             type: 'post',
-            url: "https://localhost:7088/api/choferesControllers?identificacion=" + identificacion +" &nombre=" + nombre + "&apellidos=" + apellidos +"&email=" +email +"&contrasena=" +contrasena+ + "&estado=" + estado + "",
+            url: "https://localhost:7088/api/choferesControllers?identificacion=" + identificacion +" &nombre=" + nombre + "&apellidos=" + apellidos +"&email=" +email +"&contrasena=" +contrasena+ "&estado=" + estado + "",
             contentType: "application/json; charset=utf-8",
             cache: false, 
             datatype: 'jsonp',
             traditional: true,
             success: function (response) {
-                  $('#modalMensaje').text("El chofer con identificacion " + identificacion + ", nombre " + nombre + ", apellidos  " + apellidos +", email " +email + " fecha " + fecha, + " en estado " + estado + " fue agregado.");
+                  $('#modalMensaje').text("El chofer con identificacion " + identificacion + ", nombre " + nombre + ", apellidos  " + apellidos +", email " +email + " en estado " + estado + " fue agregado.");
                   $('#modalup').trigger('click');
             },
             failure: function (response) {
