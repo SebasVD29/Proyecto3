15fb10913a56ef3fb77893b8748d451e18e6fc90
Mauricio Taylor Fonseca <mauriciotf94@gmail.com>
2023-10-22 21:23:22 -0600

    camiones crud
    camiones crud

---
+++ Proyecto3Web/API Choferes/Controllers/camionesControllers.cs
@@ -0,0 +1,167 @@
+﻿using API_Choferes.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Http.Cors;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Choferes.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [EnableCors(origins: "*", methods: "*", headers: "*")]
+    public class camionesControllers : ControllerBase
+    {
+       
+        private DataBaseController dataBase;
+        private SqlConnection conexion;
+
+
+        int count = 0;
+
+        public camionesControllers()
+        {
+            this.dataBase = new DataBaseController();
+            this.conexion = new SqlConnection(this.dataBase.StringConexion());
+        }
+        // GET: api/<camionesControllers>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {         
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<camionesControllers>/5
+        [HttpGet("{numeroPlaca}")]
+        public string[] Get(string numeroPlaca)
+        {
+            
+            try
+            {
+                
+                string[] returnValues = new string[100];
+                //int counter = 0;
+
+                this.conexion.Open();
+
+                string querySQL = "Select * from dbo.Camiones where numerPlaca = @numeroPlaca";
+
+                using (SqlCommand comando = new SqlCommand(querySQL, this.conexion))
+                {
+                    using (SqlDataReader lector = comando.ExecuteReader())
+                    {
+                        while (lector.Read())
+                        {
+                            return new string[] { (string)lector["Marca"], (string)lector["Modelo"], (DateOnly)lector["Fabricacion"], (string)lector["Estado"] };
+
+                        }
+
+                        lector.Close();
+                    }
+                    this.conexion.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return new string[] { "error", "error" };
+        }
+
+        // POST api/<camionesControllers>
+        [HttpPost]
+        [EnableCors(origins: "*", methods: "*", headers: "*")]
+        public void Post(string numeroPlaca, string Marca, string Modelo, DateOnly Fabricacion, string Estado)
+        {
+            DateTime fecha = DateTime.Now;
+           
+            try
+            {
+                this.conexion.Open();
+                string[] returnValues = new string[100];
+                string querySQL =
+                    "INSERT INTO dbo.Camiones(numeroPlaca, Marca, Modelo, Fabricacion, Estado) " +
+                    "VALUES (@numeroPlaca, @Marca, @Modelo, @Fabricacion, @Estado)";
+
+                using (SqlCommand comando = new SqlCommand(querySQL, this.conexion))
+                {
+             
+                    comando.Parameters.AddWithValue("numeroPlaca", numeroPlaca);
+                    comando.Parameters.AddWithValue("Marca", Marca);
+                    comando.Parameters.AddWithValue("Modelo", Modelo);
+                    comando.Parameters.AddWithValue("Fabricacion", Fabricacion);
+                    comando.Parameters.AddWithValue("Estado", Estado);
+                    comando.ExecuteNonQuery();
+
+
+                    /*SqlDataReader reader = comando.ExecuteReader();
+                    while (reader.Read())
+                    { }
+                    reader.Close();*/
+                }
+                this.conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return;
+        }
+
+        // PUT api/<camionesControllers>/5
+        [HttpPut("{numeroPlaca}")]
+        public void Put(string numeroPlaca, string Marca, string Modelo, DateTime Fabricacion,  string Estado)
+        {
+           
+            try
+            {
+
+                this.conexion.Open();
+                string[] returnValues = new string[100];
+                string querySQL =
+                    "UPDATE dbo.Camiones SET  Marca = @Marca, Modelo = @Modelo,Fabricacion = @Fabricacion, Estado = @Estado " +
+                    "WHERE  numeroPlaca = " + numeroPlaca;
+
+                using (SqlCommand comando = new SqlCommand(querySQL, this.conexion))
+                {
+                    
+                    comando.Parameters.AddWithValue("numeroPlaca", numeroPlaca);
+                    comando.Parameters.AddWithValue("Marca", Marca);
+                    comando.Parameters.AddWithValue("Modelo", Modelo);
+                    comando.Parameters.AddWithValue("Fabricacion", Fabricacion);
+                    comando.Parameters.AddWithValue("Estado", Estado);
+                    comando.ExecuteNonQuery();
+
+
+                   
+                }
+                this.conexion.Close();
+
+                /*SqlDataReader reader = comando.ExecuteReader();
+                while (reader.Read())
+                { }
+                reader.Close();*/
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return;
+
+
+        }
+
+        // DELETE api/<camionesControllers>/5
+        [HttpDelete("{numeroPlaca}")]
+        public void Delete(int numeroPlaca)
+        {
+        }
+    }
+}
---
+++ Proyecto3Web/carint-html/js/logicaCamiones.js
@@ -0,0 +1,130 @@
+
+$("#crearCamion").click(function () {
+
+    $("#ConfirmarCrearCamion").css("visibility", "visible");
+    $("#editarCamion").css("visibility", "hidden");
+    $("#buscarCamion").css("visibility", "hidden");
+    $("#crearCamion").css("visibility", "hidden");
+    $("#form-numeroPlaca").css('background-color', "#ffffff52");
+    $("#form-Marca").css('background-color', "#ffffff52");
+    $("#form-Modelo").css('background-color', "#ffffff52");
+    $("#form-Fabricacion").css('background-color', "#ffffff52");
+    $("#form-numeroPlaca").prop('disabled', false);
+    $("#form-Marca").prop('disabled', false);
+    $("#form-Modelo").prop('disabled', false);
+    $("#form-Fabricacion").prop('disabled', false);
+    $("#form-estado").prop('disabled', false);
+});
+
+$("#ConfirmarCrearCamion").click(function () {
+
+    var numeroPlaca = $('#form-numeroPLaca').val();
+    var Marca = $('#form-Marca ').val();
+    var Modelo = $('#form-Modelo ').val();
+    var Fabricacion = $('#form-Fabricacion ').val();
+    var estado = $("#form-estado").children(":selected")[0].label;
+
+    jQuery.ajax({
+        type: 'post',
+        url: "https://localhost:7088/api/camionesControllers?numeroPlaca=" + numeroPlaca + " &Marca=" + Marca + "&Modelo=" + Modelo + "&Fabricacion=" + Fabricacion + "&estado=" + estado + "",
+        contentType: "application/json; charset=utf-8",
+        cache: false,
+        datatype: 'jsonp',
+        traditional: true,
+        success: function (response) {
+            $('#modalMensaje').text("El Camion con el numero de placa " + numeroPlaca + ", Marca " + Marca + ", Modelo  " + Modelo + ", fabricado en " + Fabricacion + " en estado " + estado + " fue agregado.");
+            $('#modalup').trigger('click');
+        },
+        failure: function (response) {
+            alert("Error: Camion No Agregado")
+        }
+    });
+
+});
+
+$("#buscarCamion").click(function () {
+
+    $("#ConfirmarCrearCamion").css("visibility", "hidden");
+    $("#ConfirmarBuscarCamion").css("visibility", "visible");
+    $("#editarCamion").css("visibility", "hidden");
+    $("#crearCamion").css("visibility", "hidden");
+    $("#form-numeroPlaca").prop('disabled', false);
+    $("#form-numeroPlaca").css('background-color', "#ffffff52");
+    $("#form-Marca").css('background-color', "#f0f0f0");
+    $("#form-Modelo").css('background-color', "#f0f0f0");
+    $("#form-Fabricacion").css('background-color', "#f0f0f0");
+    $("#buscarCamion").css("visibility", "hidden");
+
+});
+$("#ConfirmarBuscarCamion").click(function () {
+    $("#ConfirmarBuscarCamion").css("visibility", "hidden");
+    $("#ConfirmarCrearCamion").css("visibility", "hidden");
+    $("#buscarCamion").css("visibility", "hidden");
+    $("#ConfirmarEditarCamion").css("visibility", "visible");
+    $("#crearCamion").css("visibility", "hidden");
+    $("#editarCamion").css("visibility", "visible");
+    $("#form-numeroPlaca").css('background-color', "#f0f0f0");
+    $("#form-Marca").css('background-color', "#ffffff52");
+    $("#form-Modelo").css('background-color', "#ffffff52");
+    $("#form-Fabricacion").css('background-color', "#ffffff52");
+    $("#form-numeroPlaca").prop('disabled', true);
+    $("#form-Marca").prop('disabled', false);
+    $("#form-Modelo").prop('disabled', false);
+    $("#form-Fabricacion").prop('disabled', false);
+    $("#form-estado").prop('disabled', false);
+});
+
+$("#ConfirmarBuscarCamion").click(function () {
+
+    var numeroPlaca = $('#form-numeroPlaca').val();
+  
+
+    jQuery.ajax({
+        type: 'get',
+        url: "https://localhost:7088/api/camionesControllers/" + numeroPlaca,
+        contentType: "application/json; charset=utf-8",
+        cache: false,
+        datatype: 'jsonp',
+        traditional: true,
+        success: function (response) {
+            $("#form-numeroPlaca").prop('disabled', true);
+            $('#form-Marca').val(response[0]);
+            $('#form-Modelo').val(response[1]);
+            $('#form-Fabricacion').val(response[2]);
+            $('#form-estado').val(0).change();
+            $('#modalMensaje').text("El Camion con numeroPlaca " + numeroPlaca + ", Marca " + Marca + ", Modelo  " + Modelo + ", Fabricacion " + Fabricacion + " en estado " + estado + " fue agregado.");
+            $('#modalup').trigger('click');
+        },
+        failure: function (response) {
+            alert("Error: Camion no encontrado")
+        }
+    });
+
+});
+
+$("#editarCamion").click(function(){
+
+      var numeroPlaca = $('#form-numeroPlaca').val();
+      var Marca = $('#form-Marca ').val();
+      var Modelo = $('#form-Modelo ').val();
+      var Fabricacion = $('#form-Fabricacion ').val();
+      var estado = $("#form-estado").children(":selected")[0].label;
+      
+       jQuery.ajax({
+                  type: 'put',
+           url: "https://localhost:7088/api/camionesControllers/" + numeroPlaca + "?numeroPlaca=" + numeroPlaca + "&Marca=" + Marca + "&Modelo=" + Modelo +"&Fabricacion=" +Fabricacion + "&estado=" + estado + "",
+                  contentType: "application/json; charset=utf-8",
+                  cache: false, 
+                  datatype: 'jsonp',
+                  traditional: true,
+                  success: function (response) {
+                        $('#modalMensaje').text("El Camion con el número de placa " + numeroPlaca + ", Marca " + Marca + ", Modelo  " + Modelo +", Fabricacion " +Fabricacion  + " en estado " + estado + " fue actualizado.");
+                        $('#modalup').trigger('click');
+                  },
+                  failure: function (response) {
+                        alert("Error: Camion No Actualizado")
+                  }
+              });
+      
+      });
+
