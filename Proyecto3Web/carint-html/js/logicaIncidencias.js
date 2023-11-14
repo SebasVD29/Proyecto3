8e5a4fa6c9abffa794e9d099ab326a22fdb3a2e3
Raquelmorasalazar <93454901+Raquelmorasalazar@users.noreply.github.com>
2023-11-13 19:44:13 -0600

    cambios incidencias

--- Proyecto3Web/carint-html/js/logicaIncidencias.js
+++ Proyecto3Web/carint-html/js/logicaIncidencias.js
@@ -1,4 +1,4 @@
-function insertNewRecord(data) {
+/*function insertNewRecord(data) {
     var table = document.getElementById("clientList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     newRow.insertCell(0).innerHTML = data.fullName;
@@ -8,12 +8,12 @@ function insertNewRecord(data) {
     newRow.insertCell(4).innerHTML = data.email;
     newRow.insertCell(5).innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
 }
-
+*/
 $("#ConfirmarBuscarIncidencia").click(function () {
     var ruta = $('#form-Ruta').val();
     var fechaInicio = $('#form-fechaInicio').val();
     var fechaFinal = $('#form-fechaFinal').val();
-    console.log(fechaInicio, fechaFinal);
+    console.log(ruta,fechaInicio, fechaFinal);
     $.ajax({
         type: 'get',
         url: "https://localhost:7088/api/incidentesControllers/" + ruta + "?fechaInicio=" + fechaInicio + "&fechaFinal=" + fechaFinal +"",
@@ -23,20 +23,23 @@ $("#ConfirmarBuscarIncidencia").click(function () {
         dataType: 'json', // Corregido: 'jsonp' por 'json'
         traditional: true,
         success: function (response) {
-            var table = $('#incidenciasList tbody');
+            var table = $('#incidenciasList');
             table.empty(); // Limpiar el contenido actual de la tabla
-
+           
             // Iterar sobre la lista de respuestas
-            $.each(response, function (index, incidente) {
+            $.each(response) {
                 var newRow = $("<tr>");
-                newRow.append("<td>" + incidente[0] + "</td>");
-                newRow.append("<td>" + incidente[1] + "</td>");
-                newRow.append("<td>" + incidente[2] + "</td>");
-                newRow.append("<td>" + incidente[3] + "</td>");
-                newRow.append("<td>" + incidente[4] + "</td>");
+                newRow.append("<td>" + response[0] + "</td>");
+                newRow.append("<td>" + response[1] + "</td>");
+                newRow.append("<td>" + response[2] + "</td>");
+                newRow.append("<td>" + response[3] + "</td>");
+                newRow.append("<td>" + response[4] + "</td>");
                 table.append(newRow);
             });
+           
         },
+
+
         error: function () {
             alert("Error: Incidente no encontrado");
         }
