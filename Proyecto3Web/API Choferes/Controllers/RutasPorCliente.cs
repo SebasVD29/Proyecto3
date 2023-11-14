f0aacb5674e39958db13cacfc4ada58621bac891
SebasVD29 <100324444+SebasVD29@users.noreply.github.com>
2023-11-13 19:04:00 -0600

    cambioDeRutas

--- Proyecto3Web/API Choferes/Controllers/RutasPorCliente.cs
+++ Proyecto3Web/API Choferes/Controllers/RutasPorCliente.cs
@@ -14,28 +14,36 @@ namespace API_Choferes.Controllers
     [ApiController]
     public class RutasPorCliente : ControllerBase
     {
-      
+        private securityController securityController;
         private DataBaseController dataBase;
         private SqlConnection conexion;
         public RutasPorCliente()
         {
             this.dataBase = new DataBaseController();
+            this.securityController = new securityController();
             this.conexion = new SqlConnection(this.dataBase.StringConexion());
         }
 
+
+        // GET: api/<RutasPorCliente>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { };
+        }
+
+        // GET api/<RutasPorCliente>/5
+        // Get rutas por cliente
+        // GET api/<RutasController>/5
         [HttpGet("{id}")]
-        public string[][] ObtenerDireccionRuta(int id)
+        public string[][] Get(int id)
         {
             try
             {
                 this.conexion.Open();
+
                 // Cantidad de rutas 
-                string querySQL = "SELECT DireccionRuta.NombreDireccionRuta FROM [dbo].Clientes, [dbo].DireccionRuta " +
-                    "INNER JOIN [dbo].ClientePais ON " +
-                    "DireccionRuta.IdClientePais = ClientePais.IdClientePais " +
-                    "WHERE ClientePais.IdCliente = @id " +
-                    "AND Clientes.IdentificadorCliente = ClientePais.IdCliente " +
-                    "AND Clientes.Estado = 1";
+                string querySQL = "SELECT  *\r\nFROM [dbo].[Ruta]\r\nINNER JOIN Clientes\r\nON Ruta.IdCliente = Clientes.IdentificadorCliente\r\nWHERE Ruta.IdCliente = @id AND Clientes.Estado = 1";
                 int cantidadRutas = 0;
                 using (SqlCommand comando = new SqlCommand(querySQL, this.conexion))
                 {
@@ -53,17 +61,7 @@ namespace API_Choferes.Controllers
                 }
                 string[][] returnValues = new string[cantidadRutas][];
                 int contador = 0;
-                
-                querySQL = "SELECT DireccionRuta.idDireccionRuta, DireccionRuta.NombreDireccionRuta, " +
-                    "Pais.Nombre as NombrePais, Ciudad.Nombre as NombreCiudad " +
-                    "FROM  [dbo].DireccionRuta " +
-                    "INNER JOIN [dbo].ClientePais ON DireccionRuta.IdClientePais = ClientePais.IdClientePais " +
-                    "INNER JOIN [dbo].PaisCiudad ON DireccionRuta.IdPaisCiudad = PaisCiudad.IdPaisCiudad " +
-                    "INNER JOIN [dbo].Pais ON Pais.IdentificadorPais = PaisCiudad.IdPais " +
-                    "INNER JOIN [dbo].Ciudad ON Ciudad.IdentificadorCiudad = PaisCiudad.IdCiudad " +
-                    "INNER JOIN [dbo].Clientes ON Clientes.IdentificadorCliente = ClientePais.IdCliente " +
-                    "WHERE Clientes.IdentificadorCliente =  @id " +
-                    "AND Clientes.Estado = 1";
+                querySQL = "SELECT  *\r\nFROM [dbo].[Ruta]\r\nINNER JOIN Clientes\r\nON Ruta.IdCliente = Clientes.IdentificadorCliente\r\nWHERE Ruta.IdCliente = @id AND Clientes.Estado = 1";
                 using (SqlCommand comando = new SqlCommand(querySQL, this.conexion))
                 {
 
@@ -73,10 +71,7 @@ namespace API_Choferes.Controllers
                         while (lector.Read())
                         {
                             // Devolver array en lugar de primer elemento 
-                            returnValues[contador] = new string[] { 
-                                ((int)lector["idDireccionRuta"]).ToString(), 
-                                (string)lector["NombreDireccionRuta"]
-                            };
+                            returnValues[contador] = new string[] { ((int)lector["idDireccionRuta"]).ToString(), (string)lector["Nombre"] };
                             contador++;
                         }
                         return returnValues;
@@ -95,6 +90,22 @@ namespace API_Choferes.Controllers
             }
         }
 
-     
+        // POST api/<RutasPorCliente>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RutasPorCliente>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RutasPorCliente>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
---
+++ Proyecto3Web/API Choferes/Controllers/rutasControllers.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Http.Cors;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Choferes.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    [EnableCors(origins: "*", methods: "*", headers: "*")]
+    public class rutasControllers : ControllerBase
+    {
+
+        
+        private DataBaseController dataBase;
+        private SqlConnection conexion;
+
+
+        int count = 0;
+
+        public rutasControllers()
+        {
+            this.dataBase = new DataBaseController();
+           
+            this.conexion = new SqlConnection(this.dataBase.StringConexion());
+        }
+        // GET: api/<choferesControllers>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {         
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<choferesControllers>/5
+        [HttpGet("{id}")]
+        public string[] Get(int id)
+        {
+            
+            try
+            {
+                string[] returnValues = new string[100];
+                //int counter = 0;
+                this.conexion.Open();
+                string querySQL = "";
+
+                using (SqlCommand comando = new SqlCommand(querySQL, this.conexion))
+                {
+                    comando.Parameters.AddWithValue("@id", id);
+                    using (SqlDataReader lector = comando.ExecuteReader())
+                    {
+                        while (lector.Read())
+                        {
+                            //return ;
+
+                        }
+
+                        lector.Close();
+                    }
+                    this.conexion.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return new string[] { "error", "error" };
+        }
+
+        // POST api/<choferesControllers>
+        [HttpPost]
+        [EnableCors(origins: "*", methods: "*", headers: "*")]
+        public void Post()
+        {
+            
+            
+
+            try
+            {
+                this.conexion.Open();
+                string[] returnValues = new string[100];
+                string querySQL =
+                    "INSERT INTO dbo.() " +
+                    "VALUES ()";
+
+                using (SqlCommand comando = new SqlCommand(querySQL, this.conexion))
+                {
+                    
+                    comando.ExecuteNonQuery();
+
+
+                    
+                }
+                this.conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return;
+        }
+
+        // PUT api/<choferesControllers>/5
+        [HttpPut("{id}")]
+        public void Put()
+        {
+            
+
+            try
+            {
+                
+                this.conexion.Open();
+                string[] returnValues = new string[100];
+                string querySQL =
+                    "UPDATE dbo. SET  " +
+                    "WHERE  "  ;
+
+                using (SqlCommand comando = new SqlCommand(querySQL, this.conexion))
+                {
+                    
+                    
+                    comando.ExecuteNonQuery();
+
+
+                   
+                }
+                this.conexion.Close();
+
+                /*SqlDataReader reader = comando.ExecuteReader();
+                while (reader.Read())
+                { }
+                reader.Close();*/
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return;
+
+
+        }
+
+        // DELETE api/<choferesControllers>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
